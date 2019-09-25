4fa634861100192cdc28d3a22c5625cecd717231
Sandra Roques <roquesandra@hotmail.fr>
2019-09-25 14:50:44 +0200

    jeu simple fonctionnel

--- Play.java
+++ Play.java
@@ -17,6 +17,7 @@ public class Play {
 
 		//Message d'accueil
 		System.out.println("Do you want to play ?");
+
 		System.out.println("Choose your option :");
 		System.out.println("1. Yes");
 		System.out.println("2. No");
@@ -25,81 +26,63 @@ public class Play {
 		//Début du jeu
 		if (choice == 1) {
 
-			//Tirage des cartes
-		int playerCard_1 = drawOneCard();
-		int playerCard_2 = drawOneCard();
-		int playerScore = playerCard_1 + playerCard_2;
+				//Tirage des cartes
+			int playerCard_1 = drawOneCard();
+			int playerCard_2 = drawOneCard();
+			int playerScore = playerCard_1 + playerCard_2;
 
 
-		int dealerCard_1 = drawOneCard();
-		int dealerCard_2 = drawOneCard();
-		int dealerScore = dealerCard_1 + dealerCard_2;
+			int dealerCard_1 = drawOneCard();
+			int dealerCard_2 = drawOneCard();
+			int dealerScore = dealerCard_1 + dealerCard_2;
+	
+				//Affichage des résultats
+			System.out.println("\nFIRST ROUND\n");
+			System.out.println("Your score : " + playerScore);
+			System.out.println("Dealer score : " + dealerCard_1);
 
-			//Affichage des résultats
-		System.out.println("Votre score est de " + playerScore);
-		System.out.println("La banque a un score de " + dealerCard_1);
+			while (choice == 1) {
+					//Continuer ?
+				System.out.println("\nDo you want to take another card ?\n");
+				System.out.println("Choose your option :");
+				System.out.println("1. Yes");
+				System.out.println("2. No\n");
+				choice = sc.nextInt();
 
-			//Continue ?
-		System.out.println("Do you want to take another card ?");
-		System.out.println("Choose your option :");
-		System.out.println("1. Yes");
-		System.out.println("2. No");
-		choice = sc.nextInt();
+				if (choice == 1) {
+					System.out.println("\nNEW ROUND\n");
+					int playerCard_3 = drawOneCard();
+					playerScore += playerCard_3;
 
-				//Continuer
-			if (choice == 1) {
-				System.out.println("jouer encore");
-				int playerCard_3 = drawOneCard();
-				playerScore += playerCard_3;
+					System.out.println("Your score : " + playerScore);
+					System.out.println("Dealer score : " + dealerCard_1);
 
-				System.out.println("Votre score est de " + playerScore);
-				System.out.println("La banque a un score de " + dealerCard_1);
-
-
-				if (playerScore < 21) {
-					System.out.println("Do you want to take another card ?");
-					System.out.println("Choose your option :");
-					System.out.println("1. Yes");
-					System.out.println("2. No");
-					choice = sc.nextInt();
-				} else if (playerScore == 21) {
-					System.out.println("Player score: " + playerScore);
-					System.out.println("Dealer score: " + dealerScore);
-					System.out.println("You win !");
-				} else {
-					System.out.println("Your score is over than 21 !");
-					System.out.println("You loose !");
-				}
-
-
-
-				//Stop
+					if (playerScore > 21) {
+						choice = 2;
+					}
+				} 
+			} 
+			//Comparaison des résultats
+			if (playerScore > 21) {
+				System.out.println("YOU LOOSE !");
+			} else if (playerScore == 21) {
+				System.out.println("BLACK JACK ! YOU WIN !");
+			} else if (playerScore == dealerScore) {
+				System.out.println("THIS IS A DRAW !"); 
+			} else if (playerScore > dealerScore) {
+				System.out.println("YOU WIN !");
 			} else {
-				System.out.println("Comparaison des résultats");
-				if (playerScore > dealerScore) {
-					System.out.println("Player score: " + playerScore);
-					System.out.println("Dealer score: " + dealerScore);
-					System.out.println("You win !");
-				} else {
-					System.out.println("Player score: " + playerScore);
-					System.out.println("Dealer score: " + dealerScore);
-					System.out.println("You loose !");
-					
-				}
-
-
+				System.out.println("YOU LOOSE !");	
 			}
-		System.out.println("Aurevoir");
-		System.exit(0);
+			System.out.println("\nSee you next time !");
+			System.exit(0);
 
-
-		//Message de sortie
+		//Fin du jeu (Intro)
 		} else {
-			System.out.println("Aurevoir");
+			System.out.println("\nBye !");
 			System.exit(0);
 		}
 
 	}
-
 
 }
